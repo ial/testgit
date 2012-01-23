2360e16d5ccb7c8bbb545b72e6ca070e7a4c99a5
Emil <enumzzz@enumzzzpc.(none)>
2012-01-23 17:57:30 +0300

    This is my first project on github

---
+++ test_git/test_git/test_git.cpp
@@ -0,0 +1,12 @@
+// test_git.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main(int argc,char *argv[])
+{
+	std::cout << "Hello, Igor" << std::endl; 
+	return 0;
+}
+
