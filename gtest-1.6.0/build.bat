c5f1d724060b3f9131cdffd670d5d2579171dd9c
Igor <air_pc@mail.ru>
2012-01-27 20:00:29 +0400

    google test shared_crt use

--- gtest-1.6.0/build.bat
+++ gtest-1.6.0/build.bat
@@ -2,5 +2,5 @@ call vcvars32.bat
 
 md ..\build_output\google_test
 cd ..\build_output\google_test
-cmake %~dp0 -G "NMake Makefiles"
+cmake %~dp0 -G "NMake Makefiles" -Dgtest_force_shared_crt:BOOL=ON
 nmake
