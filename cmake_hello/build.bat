fbc1536e6ea329312a8393b25daec6f13287c48e
Igor <air_pc@mail.ru>
2012-01-27 20:07:31 +0400

    unittest added for hash_map

--- cmake_hello/build.bat
+++ cmake_hello/build.bat
@@ -1,3 +1,6 @@
+call vcvars32.bat 
+
 md ..\build_output\cmake_hello
 cd ..\build_output\cmake_hello
-cmake %~dp0
+cmake %~dp0 -G "NMake Makefiles"
+nmake
---
+++ cmake_hello/gen_project.bat
@@ -0,0 +1,3 @@
+md ..\build_output\cmake_hello_prj
+cd ..\build_output\cmake_hello_prj
+cmake %~dp0
