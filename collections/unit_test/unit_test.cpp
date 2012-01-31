7cfcd73a704a7a260ace370635d28ae22611dd85
Igor <air_pc@mail.ru>
2012-01-31 11:32:57 +0400

    unit_test for hash_map updated

--- collections/unit_test/unit_test.cpp
+++ collections/unit_test/unit_test.cpp
@@ -8,42 +8,42 @@ typedef pair<string,int> pii;
 
 
 int main(int argc[], char *argv[])
-{
-	
+{	
 	hash_map<string,int> a;
-	a.insert(pii("123",32));
-	a.insert(pii("324",323));
-	a.insert(pii("324324",4334));
-
-  ASSERT_EQ(3, a.size());
-
-	ASSERT_TRUE(a.find(pii("123",32)));
-	ASSERT_FALSE(a.find(pii("32",3222)));
-	ASSERT_TRUE(a.find(pii("324",323)));
-
-	a.erase(pii("324",323));
-	ASSERT_FALSE(a.find(pii("324",323)));
-
-	a.insert(pii("123",123456732));             
-	ASSERT_TRUE(a.find(pii("123",32)));
-	ASSERT_TRUE(a.find(pii("123",123456732)));
-	a.insert(pii("1234",123456732));
-	ASSERT_TRUE(a.find(pii("123",123456732)));
-	ASSERT_TRUE(a.find(pii("1234",123456732)));
-	a.erase(pii("123",123456732));
-	ASSERT_FALSE(a.find(pii("123",123456732)));
-	ASSERT_TRUE(a.find(pii("1234",123456732)));
-
-	a.erase(pii("324",323));
-
-  string s = "";
-  for (int i = 0; i < 10000; i += 10)
-  {
-    a.insert(pii(s, i));
-    ASSERT_TRUE(a.find(pii(s, i)));
-    s += "s";
-  }
-  std::cout << s << endl;
+  a.set_value("xxx", 123);
+  EXPECT_EQ(123, a.get_value("xxx"));
+  a.set_value("xxx", 124);
+  EXPECT_EQ(124, a.get_value("xxx"));
+  a.erase("xxx");
+  EXPECT_FALSE(a.find("xxx"));
+  a.set_value("xxx", 125);
+  EXPECT_EQ(125, a.get_value("xxx"));
+  EXPECT_EQ(1, a.size());
   
+	hash_map<string,int> b;
+  b = a;
+  EXPECT_EQ(1, b.size());
+  a.erase("xxx");
+  EXPECT_EQ(1, b.size());
+  EXPECT_NO_FATAL_FAILURE(a.erase("xxx"));
+
+	hash_map<string,int> c(b);
+  EXPECT_EQ(1, c.size());
+  b.erase("xxx");
+  EXPECT_EQ(1, c.size());
+
+	hash_map<int,int> ih;
+  for (int i=0;i<100000;i++)
+  {
+    ih.set_value(i, i);
+  }
+  for (int i=100000-1;i>=0;i--)
+  {
+    EXPECT_TRUE(ih.find(i));
+    ih.erase(i);
+    EXPECT_FALSE(ih.find(i));
+  }
+  EXPECT_EQ(0, ih.size());
+
   return 0;
 }
