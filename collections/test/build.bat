c995416bea1a55e379c17522eea4eab8615469d5
Igor <air_pc@mail.ru>
2012-01-31 14:09:26 +0400

    test removed

--- collections/test/build.bat
+++
@@ -1,6 +0,0 @@
-call vcvars32.bat 
-
-md ..\..\build_output\hash_map_test
-cd ..\..\build_output\hash_map_test
-cmake %~dp0 -G "NMake Makefiles"
-nmake
--- collections/test/gen_project.bat
+++
@@ -1,3 +0,0 @@
-md ..\build_output\hash_map_prj
-cd ..\build_output\hash_map_prj
-cmake %~dp0
